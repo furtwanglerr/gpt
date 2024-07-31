4aed56d6448c29a03b5b1a2e06bd132898b7a926
furtwanglerr <140227918+furtwanglerr@users.noreply.github.com>
2024-07-31 14:41:48 +0900

    Update imagine.js

--- src/commands/imagine.js
+++ src/commands/imagine.js
@@ -26,19 +26,19 @@ module.exports = {
 
       const row = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
-          .setLabel(`Download`)
+          .setLabel(`다운로드`)
           .setStyle(ButtonStyle.Link)
           .setURL(`${output[0]}`)
           .setEmoji('1101133529607327764')
       );
 
       const resultEmbed = new EmbedBuilder()
-        .setTitle('Image Generated')
+        .setTitle('이미지 생성됨')
         .addFields({ name: 'Prompt', value: prompt })
         .setImage(output[0])
         .setColor('#44a3e3')
         .setFooter({
-          text: `Requested by ${interaction.user.username}`,
+          text: `요청자: ${interaction.user.username}`,
           iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
         });
 
@@ -48,7 +48,7 @@ module.exports = {
       });
     } catch (error) {
       const errEmbed = new EmbedBuilder()
-        .setTitle('An error occurred')
+        .setTitle('에러')
         .setDescription('```' + error + '```')
         .setColor(0xe32424);
 
@@ -58,17 +58,17 @@ module.exports = {
 
   data: {
     name: 'imagine',
-    description: 'Generate an image using a prompt.',
+    description: '프롬프트를 작성하여 이미지 생성',
     options: [
       {
         name: 'prompt',
-        description: 'Enter your prompt',
+        description: '프롬프트 작성',
         type: ApplicationCommandOptionType.String,
         required: true,
       },
       {
         name: 'model',
-        description: 'The image model',
+        description: '이미지를 생성할 모델',
         type: ApplicationCommandOptionType.String,
         choices: models,
         required: false,
